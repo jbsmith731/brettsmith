8ed7f8e48658b9ca93e6d3647cf68478689feb48
Brett Smith <jbsmith731@gmail.com>
2026-03-25 19:40:15 -0500

    refactor(root): Remove Google Fonts links configuration

--- app/root.tsx
+++ app/root.tsx
@@ -11,19 +11,6 @@ import { Footer } from "./components/Footer";
 import type { Route } from "./+types/root";
 import "./app.css";
 
-export const links: Route.LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
-];
-
 export const meta: Route.MetaFunction = () => [
   { name: "robots", content: "noindex, nofollow" },
 ];
